247e9d9c59212b8367d6df39a70ed798a6d362b3
madhumallela99 <151830395+madhumallela99@users.noreply.github.com>
2025-12-23 19:31:08 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins → CodePipeline → ECS 🚀"
+    return "Hello from madhu → ECS 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
