034a1dc9fd78e2f8b104be589d4a8db095143edd
madhumallela99 <151830395+madhumallela99@users.noreply.github.com>
2025-12-23 19:40:28 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from madhu → ECS 🚀"
+    return "Hello from madhu   → ECS 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
 
